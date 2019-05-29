2cacfd897a01572ba1fc39ed965e448fafa4137c
hecuncun <hecuncun>
2019-05-29 14:23:01 +0800

    添加联系人搜索与最近联系页面

--- app/src/main/java/com/lhzw/searchlocmap/ui/ShortMessUploadActivity.java
+++ app/src/main/java/com/lhzw/searchlocmap/ui/ShortMessUploadActivity.java
@@ -146,7 +146,7 @@ public class ShortMessUploadActivity extends Activity implements
 			sendMesgToServer();
 			break;
 		}
-	};
+	}
 
 	private void sendMesgToServer() {
 		// TODO Auto-generated method stub
