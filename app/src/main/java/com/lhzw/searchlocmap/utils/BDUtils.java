d07587815bd54d467f03904388a372805759f6d8
xtqb <467493431@qq.com>
2019-07-02 09:20:36 +0800

    modified com mode

--- app/src/main/java/com/lhzw/searchlocmap/utils/BDUtils.java
+++ app/src/main/java/com/lhzw/searchlocmap/utils/BDUtils.java
@@ -1,8 +1,14 @@
 package com.lhzw.searchlocmap.utils;
 
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.RemoteException;
+
 import com.lhzw.searchlocmap.application.SearchLocMapApplication;
+import com.lhzw.searchlocmap.constants.Constants;
+import com.lhzw.searchlocmap.constants.SPConstants;
 import com.lhzw.uploadmms.UploadInfoBean;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -14,48 +20,78 @@ public class BDUtils {
     public static ConcurrentLinkedQueue<UploadInfoBean> uploadQueue = new ConcurrentLinkedQueue<UploadInfoBean>();
     private static boolean isRunnable = false;
     private static BDUtils instance;
+    private final Handler threadHandlerer;
 
-    public static BDUtils getInstance(){
-        if(instance == null) {
+    public static BDUtils getInstance() {
+        if (instance == null) {
             instance = new BDUtils();
         }
         return instance;
     }
 
-    public synchronized void uploadBena(UploadInfoBean bean){
+    private BDUtils() {
+        HandlerThread thread = new HandlerThread("dataCommunication");
+        thread.start();
+        threadHandlerer = new Handler(thread.getLooper());
+    }
+
+    public synchronized void uploadBena(UploadInfoBean bean) {
         uploadQueue.add(bean);
         doTask();
     }
 
-    public synchronized void uploadBena(List<UploadInfoBean> list){
+    public synchronized void uploadBena(List<UploadInfoBean> list) {
         uploadQueue.addAll(list);
         doTask();
         list.clear();
     }
 
     private void doTask() {
-        if(!isRunnable && !uploadQueue.isEmpty()) {
+        if (!isRunnable && !uploadQueue.isEmpty()) {
             isRunnable = true;
-            new Thread(new Action()).start();
+            threadHandlerer.post(new Action());
         }
     }
 
     private class Action implements Runnable {
         @Override
         public void run() {
-            try {
-                List<UploadInfoBean> uploadList = new ArrayList<>();
-                do {
-                    uploadList.add(uploadQueue.poll());
-                }while(!uploadQueue.isEmpty());
-                if (SearchLocMapApplication.getInstance() != null && SearchLocMapApplication.getInstance().getUploadService() != null) {
-                    SearchLocMapApplication.getInstance().getUploadService().doTask(uploadList);
-                }
-            } catch (RemoteException e) {
-                e.printStackTrace();
+            switch (SpUtils.getInt(SPConstants.COM_MODE, Constants.COM_MODE_BD)) {
+                case Constants.COM_MODE_BD:
+                    bdCom();
+                    break;
+                case Constants.COM_MODE_NET:
+                    netCom();
+                    break;
+                case Constants.COM_MODE_AUTO:
+                    autoCom();
+                    break;
             }
             isRunnable = false;
             doTask();
         }
     }
+
+    private void bdCom(){
+        try {
+            List<UploadInfoBean> uploadList = new ArrayList<>();
+            do {
+                uploadList.add(uploadQueue.poll());
+            } while (!uploadQueue.isEmpty());
+            if (SearchLocMapApplication.getInstance() != null && SearchLocMapApplication.getInstance().getUploadService() != null) {
+                SearchLocMapApplication.getInstance().getUploadService().doTask(uploadList);
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void netCom(){
+
+    }
+
+    public void autoCom(){
+
+    }
+
 }
